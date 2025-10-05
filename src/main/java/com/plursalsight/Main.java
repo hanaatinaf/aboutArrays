a1c0ca7612ee392d8e2cd50fb973f23434a188d4
Hana Atinaf <hanaatianf2@gmail.com>
2025-10-05 17:42:14 -0600

    change some code here

--- src/main/java/com/plursalsight/Main.java
+++ src/main/java/com/plursalsight/Main.java
@@ -1,17 +1,66 @@
 package com.plursalsight;
+import  java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*Definition: An array is a list of elements (like numbers, text, etc.) all of the same type.
+
+Key Point: The size of the array is fixed when you create it. You can't make it bigger or smaller later.*/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Hell");
+        // create and initialize at the same time
+        // this array holdes integers in this case
+        int[] scores = {77,80,99,90,65};
+        // create a empty array  this holdes 4 string
+        //This creates an array that can hold 4  Strings.
+        //All slots are initially `null` (which means "no value").
+        String[] name = new String[4];
+        System.out.println(name);
+        System.out.println("Entre the 10 num:");
+        //int[] n = scanner.nextInt();
+
+       // System.out.println(n);
+
+        //How to Access Elements in an Array
+        // we use index num and indexs start at 0
+        int atindext = scores[0];
+        System.out.println(atindext);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+        System.out.println(scores[3]);
+        System.out.println(scores[4]);
+
+        //To get the total number of elements (the length), you use .length.
+        int a =  name.length;
+        System.out.println(a);
+
+        int b =  scores.length;
+        System.out.println("the total num of elemnt we have here is :"+ b);
+
+        // How to Loop Through an Array
+        //The Standard for Loop (You have the index)
+        //This is very powerful because you know the position (index) of each element.
+        for (int i = 0; i < scores.length; i++){
+            System.out.println("The index of " + i + " has value " + scores[i]);
+
         }
+        //int i = 0;: Start at the first index (0).
+        //i < scores.length;: Keep going as long as i is less than the length (so, from 0 to 5)
+        // i++: Increase i by 1 each time.
+
+       // The "for-each" Loop (You have the value)
+       // This is simpler if you only care about the values, not their positions.
+        for (int singlen: scores){
+            System.out.println(singlen);
+        }
+
+        /*When you use the for statement to loop through an array, you have access to the index and can use that to access the
+element of an array
+When you use the for-each statement to loop through an array, you have the element in the array*/
+
+
+
+
     }
 }
--- src/main/java/com/plursalsight/PassinganArraytoAMethod.java
+++ src/main/java/com/plursalsight/PassinganArraytoAMethod.java
@@ -1,4 +1,73 @@
 package com.plursalsight;
+import java.util.Scanner;
+/*Passing arrays: When you want a method to process YOUR data (calculate average,
+find high score, etc.)
+Returning arrays: When you want a method to GIVE YOU data (get test scores from user,
+generate random numbers, etc.)*/
 
 public class PassinganArraytoAMethod {
+    public static void main(String[] args) {
+        //numbers - Passing Array to a Method
+        //We create an array called numbers with 7 values
+        //We pass this entire array to the displayarrayNum method
+        int[] numbers = {19,45,66,66,90,636,22};
+        displayarrayNum(numbers);
+        //System.out.println(numbers);
+
+        //n - Returning Array from a Method
+        // We call returnArrayfromMethods() which gives us back an array
+        // We store that returned array in a new variable n
+        // Then we loop through n to display its contents
+        int[] n = returnArrayfromMethods();
+        for (int i = 0; i<n.length; i++){
+            System.out.println("The indext num:" + i + "The value:" + n[i]);
+        }
+
+
+        String[] names = nameOfThePeople();
+        /*for (String name : names){
+            System.out.println(name);
+        }*/
+        for (int i = 0; i<names.length; i++){
+            System.out.println("The indext num:" + i + "The value:" + names[i]);
+        }
+
+    }
+    public  static  void displayarrayNum(int[] num){
+        for (int i = 0; i<num.length ; i++){
+            System.out.println("the index num is:" + i + " The value is:"+ num[i]);
+        }
+   /*The method receives the array as num (you can use any parameter name)
+   It loops through and prints both the index(i) AND the value(num[i])
+   Important: The method doesn't return anything (void) - it just does work and finishes*/
+
+    }
+    /*Returning an Array from a Method*/
+    public  static int[] returnArrayfromMethods(){
+        int[] numbers = {3,4,55,33,22,21,5554,77};
+        return  numbers;
+
+        /*Notice the return type is int[] (array of integers)
+        The method creates an array, then returns it*/
+
+    }
+    // this method will return an String array of num;
+    public static  String[] nameOfThePeople(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre the how many  name you want store:");
+        int names1 = scanner.nextInt();
+
+        scanner.nextLine();// clear the buffer
+
+        String[] name=  new String[names1];
+
+         for (int i = 0; i <name.length; i++){
+            System.out.println("The index: " + i + "The Name" + (i+ 1)+ ":");
+            name[i] = scanner.nextLine();// stores the name
+            // name.toString();
+        }
+         scanner.close();
+         return name;
+    }
 }
